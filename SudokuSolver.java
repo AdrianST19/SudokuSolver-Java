2e7f134dc8971d1590bb33bb3c81d6aa17c68abe
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-11-12 15:44:16 +0200

    Added file

---
+++ SudokuSolver.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SudokuSolver {
+	
+	private static final int GRID_SIZE = 9;
+	
+	public static void main(String[]args) {
+		
+		int[][] default_board= {
+			  { 8, 0, 0, 0, 0, 0, 0, 0, 0 },
+			  { 0, 0, 3, 6, 0, 0, 0, 0, 0 },
+			  { 0, 7, 0, 0, 9, 0, 2, 0, 0 },
+			  { 0, 5, 0, 0, 0, 7, 0, 0, 0 },
+			  { 0, 0, 0, 0, 4, 5, 7, 0, 0 },
+			  { 0, 0, 0, 1, 0, 0, 0, 3, 0 },
+			  { 0, 0, 1, 0, 0, 0, 0, 6, 8 },
+			  { 0, 0, 8, 5, 0, 0, 0, 1, 0 },
+			  { 0, 9, 0, 0, 0, 0, 4, 0, 0 } 
+		      };
+		int[][] board=new int[9][9];
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Go with the default board or input your own?\nType \"d\" for default board\n"
+				+ "Type \"i\" to introduce your own");
+		char option= input.next().charAt(0);
+		
+		if(option=='d') {
+			board=default_board;
+			printBoard(board);
+			System.out.println("The board was solved succesfully!");
+			printBoard(board);
+		}
+		else if(option=='i') {
+			int row=0;
+			for(int i=0;i<GRID_SIZE;i++) {
+				System.out.println("Insert row "+(i+1));
+				for(int j=0;j<GRID_SIZE;j++) {
+					board[row][j]= input.nextInt();
+				}
+				row++;
+			}
+			System.out.println( (solveBoard(board)? "The board was solved successfully!":"The board has no solving solution!"));
+			printBoard(board);
+		}
+		else System.out.println("Invalid choice!");
+		input.close();
+	}
+
+	private static void printBoard(int[][] board) {
+		for(int row=0;row<GRID_SIZE;row++) {
+			if(row % 3 == 0 && row != 0)//for every third row
+				System.out.println("------------");
+			for(int column=0;column<GRID_SIZE;column++) {
+				if(column % 3 == 0 && column != 0)//for every third column
+					System.out.print("|");
+				System.out.print(board[row][column]);
+			}
+			System.out.println();
+		}
+
+	}
+
+	private static boolean isNumInRow(int[][] board, int number, int row) {
+	
+		for(int i=0;i<GRID_SIZE;i++) 
+			if(board[row][i]==number) return true;
+		return false;
+	}
+	
+	private static boolean isNumInCol(int[][] board, int number, int column) {
+
+		for(int i=0;i<GRID_SIZE;i++) 
+			if(board[i][column]==number) return true;
+		return false;
+	}
+	
+	private static boolean isNumInBox(int[][] board, int number, int row, int column) {
+		int localBoxRow= row - row % 3;
+		int localBoxCol= column - column % 3;
+		
+		for(int i= localBoxRow; i<localBoxRow + 3; i++) 
+			for(int j= localBoxCol; j<localBoxCol + 3; j++) 
+				if(board[i][j]== number) return true;
+		return false;
+	}
+	
+	private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+		return !isNumInRow(board,number,row)&&!isNumInCol(board,number,column)&&
+			   !isNumInBox(board,number,row,column);
+	}
+	
+	private static boolean solveBoard(int[][] board) {
+		
+		for(int row=0; row< GRID_SIZE;row++) 
+			for(int column=0; column<GRID_SIZE;column++) 
+				if(board[row][column]== 0) {
+					for(int tryNum=1; tryNum<=GRID_SIZE; tryNum++) {
+						
+						if(isValidPlacement(board,tryNum,row,column)) {
+							board[row][column]= tryNum;
+							if(solveBoard(board)) return true;
+							else board[row][column]= 0;
+						}
+						
+					}
+					return false;
+				}
+		return true;
+	}
+}
